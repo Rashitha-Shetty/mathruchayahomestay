7126f5324bb739f1b99f16ff6fb0e0bf35fe27fe
Rashitha-Shetty <shettyrashitha22@gmail.com>
2023-09-24 18:29:35 +0530

    removd secret

--- bhomestay/settings.py
+++ bhomestay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(&c$ltc&3tc1+m(3k9gu)2$81-g+ul#%ypxsh$9v-qnjm_abec'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mathruchayabeachhomestay.com','www.mathruchayabeachhomestay.com']
 
 
 # Application definition
