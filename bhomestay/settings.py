260d5e9b22b86ac2a25f7e66dc56ce590229bbd6
Rashitha-Shetty <shettyrashitha22@gmail.com>
2023-10-08 15:26:55 +0530

    testing

--- bhomestay/settings.py
+++ bhomestay/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mathruchayabeachhomestay.com','www.mathruchayabeachhomestay.com']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['mathruchayabeachhomestay.com','www.mathruchayabeachhomestay.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
