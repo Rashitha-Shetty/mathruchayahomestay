6164c2b5668366304e105053c91d725e413a4d55
Rashitha-Shetty <shettyrashitha22@gmail.com>
2023-11-01 20:17:21 +0530

    addedvideos

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -16,12 +16,12 @@ generateListItems();
 
 
 
- // JavaScript function to show and hide the message box
- function showMessage() {
+// JavaScript function to show and hide the message box
+function showMessage() {
   var messageBox = document.getElementById('messageBox');
-  messageBox.style.display = 'block';
-  setTimeout(function() {
-      messageBox.style.display = 'none';
+  // messageBox.style.display = 'block';
+  setTimeout(function () {
+    // messageBox.style.display = 'none';
   }, 5000); // 5000 milliseconds = 5 seconds
 }
 
